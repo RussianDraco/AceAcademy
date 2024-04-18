12be95f778ed6f6c2d83181decd049e361c2a9b5
unknown <dracospy369@gmail.com>
2024-04-18 13:51:30 -0400

    minor

--- main.py
+++ main.py
@@ -757,10 +757,10 @@ catroom = cat_room()
 
 #Top bar buttons
 homebutton = Button(10, 10, 150, 30, "Home", home_section, overrideColour=BACKGROUND)
-flashbutton = Button(600, 10, 150, 30, "Flashcards", flashcardsection.open_flashcard_section, overrideColour=BACKGROUND)
+flashbutton = Button(xaxis_centering(150), yaxis_centering(30), 150, 30, "Flashcards", flashcardsection.open_flashcard_section, overrideColour=BACKGROUND)
 studytimerbutton = Button(330, 10, 150, 30, "Study Timer", studytimersection.open_studytimer_section, overrideColour=BACKGROUND)
 journalbutton = Button(770, 10, 150, 30, "Journal", journal.open_journal_section, overrideColour=BACKGROUND)
-supportbutton = Button(800, 10, 150, 30, "Support Room", catroom.open_cats, overrideColour=BACKGROUND)
+supportbutton = Button(600, 10, 150, 30, "Support Room", catroom.open_cats, overrideColour=BACKGROUND)
 
 settingsbutton = Button(1040, 10, 150, 30, "Settings", settings.open_settings)
 
